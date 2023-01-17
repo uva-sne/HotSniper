5fdc508de563e44e72fab03482999a96f9541329
Simon Polstra <s.polstra@uva.nl>
2023-01-17 11:54:53 +0100

    back to 8x8 cores platform, still getting the exception.

--- tools/mcpat.py
+++ tools/mcpat.py
@@ -717,7 +717,11 @@ def power_stack(power_dat, cfg, seconds, powertype='total', nocollapse=False):
             hotspot_args += ['-init_file', os.path.join(
                 sniper_config.get_config(cfg, "general/output_dir"), 'Temperature.init')]
 
+        # SP: debug
+        print([hotspot_binary] + hotspot_args)
         temperatures = subprocess.check_output([hotspot_binary] + hotspot_args)
+        print(Temperatures)
+
         with open(os.path.join(sniper_config.get_config(cfg, "general/output_dir"), 'Temperature.init'), 'w') as f:
             f.write(temperatures)
 
